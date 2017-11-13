6b25cebd4390af4333e5b29de13b443fff61a35e
ES <evgeniy.saschenko@gmail.com>
2017-11-13 17:27:27 +0200

    git pages

---
+++ diet-pills/js/bandle.js
@@ -0,0 +1,1 @@
+(function(){$(document).ready(function(){var t;return $(".form-product__item_1").addClass("active"),t=$(".form-product__item_1").find('[type="radio"]'),t.attr("checked","checked"),$('[type="radio"]').on("change",function(){return $(".form-product__item").removeClass("active"),$(this).parents(".form-product__item").addClass("active")})})}).call(this),function(){}.call(this);
---
+++ mondaystydio/js/mondaystydio.js
@@ -0,0 +1,64 @@
+(function() {
+  $(document).ready(function() {
+    $('.calculator .list-options .item').each((function(_this) {
+      return function(i, el) {
+        var icon;
+        icon = $(el).data('icon');
+        if (icon) {
+          return $(el).addClass('active');
+        }
+      };
+    })(this));
+    return $('.calculator .list-options .item').on('click', function() {
+      var active;
+      active = $(this).hasClass('active');
+      if (active) {
+        return $(this).removeClass('active');
+      } else {
+        $(this).addClass('active');
+        return $(this).children('.item__icon').addClass('icon-check-white');
+      }
+    });
+  });
+
+}).call(this);
+
+(function() {
+  $(document).ready(function() {
+    var widthNavList;
+    $('.header .nav-toggler').on('click', function() {
+      var active;
+      active = $(this).parent().hasClass('active');
+      if (active) {
+        return $(this).parent().removeClass('active');
+      } else {
+        return $(this).parent().addClass('active');
+      }
+    });
+    $('.nav a').on('click', function() {
+      $('.nav a').removeClass('active');
+      return $(this).addClass('active');
+    });
+    $('.header .nav a').on('click', function() {
+      var id, top;
+      event.preventDefault();
+      id = $(this).attr('href');
+      top = $(id).offset().top;
+      return $('body,html').animate({
+        scrollTop: top
+      }, 1000);
+    });
+    widthNavList = function() {
+      var winWidth;
+      winWidth = $(window).width();
+      if (winWidth <= 700) {
+        return $('.header .nav-list').css('width', winWidth + 'px');
+      }
+    };
+    $(window).resize(function() {
+      return widthNavList();
+    });
+    return widthNavList();
+  });
+
+}).call(this);
